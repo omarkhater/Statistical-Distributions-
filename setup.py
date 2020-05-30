bd5a263fbe0be14d940ca9709445ba84e9f39076
omarkhater <omer_ayman2009@yahoo.com>
2020-05-30 22:40:16 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author: %(OmarKhater)s
+"""
+
+
+from setuptools import setup
+setup (name = 'distributions',
+    version = '.1',
+    description = 'Gaussian distribution',
+    packages = ['distributions'],
+    zip_safe = False)
